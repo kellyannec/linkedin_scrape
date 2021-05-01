95c63f231a396afdf27e79321abddcb7f44bd58c
kellyannec <83430581+kellyannec@users.noreply.github.com>
2021-05-01 18:03:05 -0400

    Add files via upload
    V1 - works for basic profiles with experience and education listed as one position per company. If a profile lists a company with multiple positions that experience is skipped over for the next company & position pairing that is 1:1. Working on a new version that will identify multiple jobs @ one company and properly pull that company and most recent position.

---
+++ parameters.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 23 14:54:49 2021
+
+@author: Kelly
+"""
+
+# search query 
+search_query = 'site:linkedin.com/in/ "business"'
+
+
+# file were scraped profile information will be stored  
+file_name = 'results_file1.csv'
+
+# login credentials
+linkedin_username = 'EMAIL'
+linkedin_password = 'PASSWORD'
